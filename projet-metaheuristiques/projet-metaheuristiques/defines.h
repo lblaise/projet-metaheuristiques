47a7ab0c577eca6c379f30414a10526372869409
lblaise <lea.blaise@telecom-paristech.fr>
2017-11-05 13:15:31 +0100

    Browsing neighbourhood in a random order

--- projet-metaheuristiques/projet-metaheuristiques/defines.h
+++ projet-metaheuristiques/projet-metaheuristiques/defines.h
@@ -3,9 +3,9 @@
 #define NB_ITER 100
 #define R_CAPT 1
 #define R_COM 1
-#define NB_ROWS 10
-#define NB_COLS 10
+#define NB_ROWS 50
+#define NB_COLS 50
 #define ALPHA 1
 #define BETA 1
-#define LEN_TABOO 3
+#define LEN_TABOO 50
 #define FREQUENT 5
